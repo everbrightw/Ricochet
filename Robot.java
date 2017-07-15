3f3793b0ec9585a15fad1914e1244b09a46de596
zelinlv <lvzelin0408@gmail.com>
2017-07-14 22:07:07 -0500

    Some basic classes for this game

---
+++ Robot.java
@@ -0,0 +1,16 @@
+
+public class Robot {
+	int positionX;
+	int positionY;
+	int color; //use number as a color to differentiate robots
+	
+	Robot(int x, int y, int color){
+		positionX = x;
+		positionY = y;
+		this.color = color;
+	}
+	
+	void move (int directX, int directY){
+		
+	}
+}
